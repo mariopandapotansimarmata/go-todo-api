d3e8c56c9e5ebb18856bf4b00a90ecbfd6826a13
mariopandapotansimarmata <kurcisandiwara@gmail.com>
2024-09-08 10:04:01 +0700

    add some controller

--- controller/todo_controller_impl.go
+++ controller/todo_controller_impl.go
@@ -6,6 +6,7 @@ import (
 	"http-basic/model/web"
 	"http-basic/service"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -22,31 +23,111 @@ func (todoController *TodoControllerImpl) Create(w http.ResponseWriter, r *http.
 
 	todoResponse := todoController.TodoService.Create(r.Context(), TodoCreateRequest)
 
-	webRepsonse := web.WebResponse{Code: http.StatusOK, Status: http.StatusText(http.StatusOK), Data: todoResponse}
+	webRepsonse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+		Data:   todoResponse,
+	}
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(webRepsonse)
+	helper.PanicIfErr(err)
+}
+
+func (todoController *TodoControllerImpl) Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	decoder := json.NewDecoder(r.Body)
+	todoUpdateRequest := web.TodoUpdateRequest{}
+	err := decoder.Decode(&todoUpdateRequest)
+	helper.PanicIfErr(err)
+
+	todoId := params.ByName("todoId")
+
+	id, err := strconv.Atoi(todoId)
+	helper.PanicIfErr(err)
+
+	todoUpdateRequest.Id = id
+
+	todoResponse := todoController.TodoService.Update(r.Context(), todoUpdateRequest)
+
+	webRepsonse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+		Data:   todoResponse,
+	}
 
 	w.Header().Add("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	err = encoder.Encode(webRepsonse)
 	helper.PanicIfErr(err)
-
-}
-
-func (todoController *TodoControllerImpl) Update(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	panic("not implemented") // TODO: Implement
 }
 
 func (todoController *TodoControllerImpl) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	panic("not implemented") // TODO: Implement
+	todoId := params.ByName("todoId")
+	id, err := strconv.Atoi(todoId)
+	helper.PanicIfErr(err)
+
+	todoController.TodoService.Delete(r.Context(), id)
+
+	webRepsonse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(webRepsonse)
+	helper.PanicIfErr(err)
 }
 
 func (todoController *TodoControllerImpl) FindById(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	panic("not implemented") // TODO: Implement
+	todoId := params.ByName("todoId")
+	id, err := strconv.Atoi(todoId)
+	helper.PanicIfErr(err)
+
+	todoResponse := todoController.TodoService.FindById(r.Context(), id)
+
+	webRepsonse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+		Data:   todoResponse,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(webRepsonse)
+	helper.PanicIfErr(err)
 }
 
 func (todoController *TodoControllerImpl) FindAll(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	panic("not implemented") // TODO: Implement
+	todoResponses := todoController.TodoService.FindByAll(r.Context())
+
+	webRepsonses := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+		Data:   todoResponses,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(webRepsonses)
+	helper.PanicIfErr(err)
 }
 
 func (todoController *TodoControllerImpl) SetFinish(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	panic("not implemented") // TODO: Implement
+	todoSetFinishRequest := web.TodoSetFinishRequest{}
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&todoSetFinishRequest)
+
+	todoResponse := todoController.TodoService.SetFinish(r.Context(), todoSetFinishRequest, todoSetFinishRequest.TimeFinish)
+
+	webRepsonse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+		Data:   todoResponse,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(webRepsonse)
+	helper.PanicIfErr(err)
 }
